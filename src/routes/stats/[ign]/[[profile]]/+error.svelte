0caa7c5d2b163886a16c3db6c70edd9c1c18b694
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-01-30 19:52:08 +0100

    feat(error): add error handling

---
+++ src/routes/stats/[ign]/[[profile]]/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import { Button } from "bits-ui";
+</script>
+
+<main class="flex h-screen w-full flex-col items-center justify-center">
+  <div class="flex w-full max-w-md flex-col items-center justify-center gap-3 rounded-lg p-6 backdrop-blur-lg backdrop-brightness-50">
+    <h1 class="text-center text-2xl font-semibold">Oops! Something went wrong</h1>
+    <p class="text-center">An error occurred while trying to fetch the stats for '{page.params.ign}'</p>
+    <p>Try again or contact us if the problem persists.</p>
+    <Button.Root href="/" class="mx-auto flex w-full max-w-fit items-center justify-center rounded-3xl bg-icon px-6 py-3 text-base font-bold uppercase text-white transition-all duration-150 [text-shadow:0_0_3px_rgba(0,0,0,.5)] hover:scale-[1.015] disabled:opacity-50 dark:text-text">Go home</Button.Root>
+  </div>
+  <p class="text-xs text-text/40">{page.status}: {page.error?.message}</p>
+</main>
