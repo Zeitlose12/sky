514ebb2e85cf343a809d8da0203027b66af10cb8
DuckySoLucky <robertkovac160@gmail.com>
2025-01-31 18:06:32 +0100

    fix: error handling

--- src/routes/stats/[ign]/[[profile]]/+page.server.ts
+++ src/routes/stats/[ign]/[[profile]]/+page.server.ts
@@ -2,23 +2,24 @@ import type { Stats as StatsType } from "$lib/types/stats";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
+type APIError = { body?: { message?: string } };
+
 export const load = (async ({ params, fetch }) => {
   const { ign, profile } = params;
 
   try {
     const data = await fetch(`/api/stats/${ign}${profile ? "/" + profile : ""}`);
-
-    if (!data.ok) {
+    if (!data.ok && data.status !== 500) {
       error(data.status, "Failed to fetch data");
     }
 
-    const user = (await data.json()) as StatsType;
+    const user = (await data.json()) as StatsType & { message?: string };
+    if (user.message) {
+      error(404, user.message);
+    }
 
-    return {
-      user
-    };
+    return { user };
   } catch (e) {
-    console.error(e);
-    error(500, "Failed to fetch data");
+    error(500, (e as APIError).body?.message ?? "Failed to fetch data");
   }
 }) satisfies PageServerLoad;
