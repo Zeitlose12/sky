ff0d1bb24c9835ef1c72f92df4d6079fa3a10f02
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-02-14 20:48:52 +0100

    feat(sentry): integrate Sentry for error tracking and performance monitoring

---
+++ src/hooks.client.ts
@@ -0,0 +1,37 @@
+import { dev } from "$app/environment";
+import { PUBLIC_SENTRY_DSN } from "$env/static/public";
+import { browserTracingIntegration, contextLinesIntegration, extraErrorDataIntegration, handleErrorWithSentry, httpClientIntegration, init, replayIntegration } from "@sentry/sveltekit";
+
+init({
+  dsn: PUBLIC_SENTRY_DSN,
+
+  tunnel: "/api/tunnel",
+
+  tracesSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // If you don't want to use Session Replay, just remove the line below:
+  integrations: [
+    replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false
+    }),
+    browserTracingIntegration(),
+    httpClientIntegration(),
+    contextLinesIntegration(),
+    extraErrorDataIntegration()
+  ],
+
+  enabled: !dev,
+  environment: dev ? "development" : "production"
+});
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,25 @@
+import { dev } from "$app/environment";
+import { PUBLIC_SENTRY_DSN } from "$env/static/public";
 import { init as resourcesInit } from "$lib/server/custom_resources";
 import { indexCollectons } from "$lib/server/db/mongo/index-collections";
 import { intializeNEURepository, parseNEURepository } from "$lib/server/helper/NotEnoughUpdates/parseNEURepository";
+import { contextLinesIntegration, extraErrorDataIntegration, handleErrorWithSentry, sentryHandle, init as sentryInit } from "@sentry/sveltekit";
 import type { ServerInit } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
 import { getPrices } from "skyhelper-networth";
 import { startMongo } from "./lib/server/db/mongo";
 import { startRedis } from "./lib/server/db/redis";
+
+sentryInit({
+  dsn: PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 1,
+
+  integrations: [contextLinesIntegration(), extraErrorDataIntegration()],
+
+  // Disable Sentry during development
+  enabled: !dev,
+  environment: dev ? "development" : "production"
+});
 
 export const init: ServerInit = async () => {
   console.log("[SkyCrypt] Starting...");
@@ -32,3 +47,5 @@ export const init: ServerInit = async () => {
 
   console.log(`[SkyCrypt] Started in ${(performance.now() - timeNow).toFixed(2)}ms`);
 };
+export const handleError = handleErrorWithSentry();
+export const handle = sequence(sentryHandle());
