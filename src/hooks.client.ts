a492367a85555f407fa6d11b9a5909ddc9ff404b
DuckySoLucky <robertkovac160@gmail.com>
2025-04-20 11:25:05 +0200

    refactor(sentry): do not samble replays at all

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -11,11 +11,11 @@ init({
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // If the entire session is not sampled, use the below sample rate to sample
   // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0,
 
   // Filter out specific errors before they are sent to Sentry
   beforeSend(event, hint) {
