686af1824c1dec108628c2bc5226fd782b900700
DuckySoLucky <robertkovac160@gmail.com>
2025-06-27 19:21:47 +0200

    feaT: skills endpoint

--- src/routes/api/v2/skills/[paramPlayer=player]/[[paramProfile]]/+server.ts
+++ src/routes/api/v2/skills/[paramPlayer=player]/[[paramProfile]]/+server.ts
@@ -1,5 +1,8 @@
 import { dev } from "$app/environment";
 import { fetchPlayer, getProfile } from "$lib/server/lib.js";
+import { getEnchanting } from "$lib/server/stats/enchanting";
+import { getFarming } from "$lib/server/stats/farming";
+import { getFishing } from "$lib/server/stats/fishing.js";
 import { getMining } from "$lib/server/stats/mining.js";
 import { json } from "@sveltejs/kit";
 
@@ -11,10 +14,18 @@ export async function GET({ params, cookies }) {
   const packs = JSON.parse(cookies.get("disabledPacks") || "[]");
 
   const mining = getMining(profile.members[paramPlayer], player, packs);
+  const farming = getFarming(profile, profile.members[paramPlayer]);
+  const enchanting = getEnchanting(profile.members[paramPlayer]);
+  const fishing = getFishing(profile.members[paramPlayer]);
 
   if (dev) {
-    console.log(`/api/mining/${paramPlayer}/${paramProfile} took ${Date.now() - timeNow}ms`);
+    console.log(`/api/skills/${paramPlayer}/${paramProfile} took ${Date.now() - timeNow}ms`);
   }
 
-  return json(mining);
+  return json({
+    mining,
+    farming,
+    enchanting,
+    fishing
+  });
 }
